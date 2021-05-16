585587cf7c0861883ebfb4752e95d007a1eae2c0
FestusMutie <mutiefestus84@gmail.com>
2021-05-16 23:49:39 +0300

    add a button for adding a new quote

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteFormComponent implements OnInit {
 
+  newQuote = new Quote(0,"","",new Date());
   constructor() { }
+
+
 
   ngOnInit(): void {
   }
