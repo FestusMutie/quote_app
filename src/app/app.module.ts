63f858344138410ab604c44a1d5f633ad5a83e3a
FestusMutie <mutiefestus84@gmail.com>
2021-05-16 23:14:34 +0300

    add date count pipe and directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { HighlightDirective } from './highlight.directive';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    QuoteDetailComponent
+    QuoteDetailComponent,
+    HighlightDirective,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,12 +9,12 @@ export class QuoteComponent implements OnInit {
   title = "quote";
   
   quotes: Quote[] = [
-    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son'),
-    new Quote(2,'Buy Cookies','I have to buy cookies for the parrot'),
-    new Quote(3,'Get new Phone Case','Diana has her birthday coming up soon'),
-    new Quote(4,'Get Dog Food','Pupper likes expensive snacks'),
-    new Quote(5,'Solve math homework','Damn Math'),
-    new Quote(6,'Plot my world domination plan','Cause I am an evil overlord'),
+    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2020,3,14)),
+    new Quote(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
+    new Quote(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2019,6,9)),
+    new Quote(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2019,6,9)),
+    new Quote(5,'Solve math homework','Damn Math',new Date(2019,6,9)),
+    new Quote(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2019,6,9)),
   ];
 
   toggleDetails(index){
@@ -22,8 +22,20 @@ export class QuoteComponent implements OnInit {
   }
 
   constructor() { }
-
-  ngOnInit(): void {
+  completeQuote(isComplete, index){
+    if (isComplete) {
+      this.quotes.splice(index,1);
+    }
   }
+  ngOnInit(){
+  }
+  deleteQuote(isComplete, index){
+    if (isComplete) {
+      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
 
+      if (toDelete){
+        this.quotes.splice(index,1)
+      }
+    }
+  }
 }
